86c1404b9bdc9ecbf7dffbe607e9d2aca07d5df3
12001734 <53322106+12001734@users.noreply.github.com>
2019-08-04 21:55:14 -0700

    Add files via upload

---
+++ 8ball.py
@@ -0,0 +1,8 @@
+import random
+def magic_ball():
+    answer=random.choice(['It is certain.','Yes - definitely.',
+    'Most likely.','Outlook good.','Ask again later.','Reply hazy, try again.',
+    'My sources say no.','Don''t count on it.'])
+    print(answer)
+
+magic_ball()
