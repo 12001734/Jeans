eb21b19b4858cff396f013a99234b4d733522f00
12001734 <53322106+12001734@users.noreply.github.com>
2019-08-06 01:03:45 -0700

    Delete 8ball.py

--- 8ball.py
+++
@@ -1,8 +0,0 @@
-import random
-def magic_ball():
-    answer=random.choice(['It is certain.','Yes - definitely.',
-    'Most likely.','Outlook good.','Ask again later.','Reply hazy, try again.',
-    'My sources say no.','Don''t count on it.'])
-    print(answer)
-
-magic_ball()
