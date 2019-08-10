af74e685237c52cb05f08a3344426f6f5e365a57
12001734 <53322106+12001734@users.noreply.github.com>
2019-08-09 20:21:39 -0700

    Add files via upload

---
+++ day_1_script.py
@@ -0,0 +1,9 @@
+import os
+import enemies from datadase
+import MURDER_DEATH_RAY
+
+    while scanning=True:
+        if enemies.hair='red' and enemies.name='Saya':
+            os.system(ACTIVATE_RAY)
+            return 'Communist Threat Neutralized'
+        else: return 'Have a nice day Sir or Ma''am'
