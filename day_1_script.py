e2e5549897eccbda767eccadbdc17e5106b42650
12001734 <53322106+12001734@users.noreply.github.com>
2019-10-09 14:17:53 -0700

    Delete day_1_script.py

--- day_1_script.py
+++
@@ -1,9 +0,0 @@
-import os
-import enemies from datadase
-import MURDER_DEATH_RAY
-
-    while scanning=True:
-        if enemies.hair='red' and enemies.name='Saya':
-            os.system(ACTIVATE_RAY)
-            return 'Communist Threat Neutralized'
-        else: return 'Have a nice day Sir or Ma''am'
